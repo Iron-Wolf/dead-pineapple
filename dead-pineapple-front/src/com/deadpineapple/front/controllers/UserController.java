ebec4f4dc6cd653742c4b1d5a026108a8b8f1559
Sofiane aziri <215546@supinfo.com>
2016-03-11 12:17:50 +0100

    Ajout de l'architecture Spring ( Dispatcher et Application Context) Pour ajouter un controller - le mettre dans le package controller, les views dans views

---
+++ dead-pineapple-front/src/com/deadpineapple/front/controllers/UserController.java
@@ -0,0 +1,30 @@
+package com.deadpineapple.front.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by saziri on 10/03/2016.
+ */
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    @RequestMapping(value="/add", method= RequestMethod.GET)
+    public String addUser(){
+        System.out.println("Invoking User");
+        return "test";
+    }
+
+    @RequestMapping(value="/add", method=RequestMethod.POST)
+    public String saveUser(Model model){
+        System.out.println("Save User");
+        return "test";
+    }
+    @RequestMapping(value="/add", method=RequestMethod.POST, params={"age = 60", "notExpert"})
+    public String saveOldUser(){
+        System.out.println("Save Old User ahahah x)");
+        return "index";
+    }
+}
---
+++ dead-pineapple-front/web/WEB-INF/views/test.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: saziri
+  Date: 10/03/2016
+  Time: 17:32
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+Controller work
+</body>
+</html>
