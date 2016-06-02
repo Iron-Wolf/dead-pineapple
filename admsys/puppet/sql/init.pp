d3a4b7f1a7ed51f114b78f84145dff49aac66419
TGerardin <thomas.gerardin@gmx.fr>
2016-06-02 21:40:50 +0200

    Add Garlera cluster configuration

--- admsys/puppet/sql/init.pp
+++ admsys/puppet/sql/init.pp
@@ -1,9 +1,28 @@
 class sql{
 
- package { 'mysql':
+ package { 'python-software-properties':
+     ensure => present,
+ }
+ 
+  package { 'galera':
+     ensure => present,
+ }
+ 
+  package { 'mariadb-galera-server':
      ensure => present,
  }
 
+ file { 'galera.cnf':
+     ensure => present,
+     path = '/etc/mysql/conf.d/galera.cnf',
+     source = [
+          "puppet:///modules/sql/galera.cnf",]
+ }
+ 
+   exec { 'run_cluster':
+    command => 'service mysql start --wsrep-new-cluster',
+    path    => '/usr/local/bin/:/bin/',
+  }
 
  service { 'postfix' :
      ensure => running,
